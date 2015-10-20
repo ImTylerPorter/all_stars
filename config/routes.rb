8cb9f6c75a8a142988c96a2c468c9edd99ce99ed
Tyler Porter <hello@cleverladder.com>
2015-10-20 17:20:37 -0500

    Added custom routes for feed & login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :statuses
 
+  devise_scope :user do 
+  	get 'signup', to: 'devise/registrations#new', as: :signup
+  	get 'login', to: 'devise/sessions#new', as: :login
+  	get 'logout', to: 'devise/sessions#destroy', as: :logout
+
+  end 
+
+
+  resources :statuses
+  get 'feed', to: 'statuses#index', as: :feed
   root to: 'statuses#index'
 
 
