d2cd498e02f397588e1934b602838c7ca8804248
Tyler Porter <hello@cleverladder.com>
2015-10-20 18:11:34 -0500

    Fixed typo

--- app/models/status.rb
+++ app/models/status.rb
@@ -2,9 +2,9 @@ class Status < ActiveRecord::Base
 
 	belongs_to :user
 
-	valitdates :content, presence: true,
+	validates :content, presence: true,
 						 length: { minimum: 2 }
 
-	valitdates :user_id, presence: true
+	validates :user_id, presence: true
 	
 end
