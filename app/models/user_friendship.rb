053ffec32277f6cd4936ebbcaf87c4ae1fdd8e48
Tyler Porter <hello@cleverladder.com>
2015-10-20 20:45:41 -0500

    Fixed Bug

--- app/models/user_friendship.rb
+++ app/models/user_friendship.rb
@@ -4,10 +4,17 @@ class UserFriendship < ActiveRecord::Base
 
 def new
   if params[:friend_id]
-    @friend = User.where(params[:friend_id])
+    @friend = User.new(friend_params)
     @user_friendship = current_user.user_friendship.new(friend: @friend)
   else
     flash[:error] = "Friend Required"
   end
 end
+
+private
+ def friend_params
+    params.require(:friend_id).permit(:name, :friend, :user_id, :friend_id)
+  end
+
+
 end
